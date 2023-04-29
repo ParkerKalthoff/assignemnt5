0394cb5ed50fc198716a40f2c595dbe1399f4b71
ParkerKalthoff <parkerlkalthoff@gmail.com>
2023-04-29 14:45:19 -0500

    test

---
+++ assignment-5/src/app/data/contact-data.ts
@@ -0,0 +1,29 @@
+import { Contact } from './../models/contact';
+
+export const CONTACT: Contact[] = [
+    {
+      firstname: "Parker",
+      lastname: "Kalthoff",
+      phonenum: 333_333_3333,
+      emailaddress: "parkerkalthoff@gmail.com",
+    },
+    {
+      firstname: "Samantha",
+      lastname: "Jones",
+      phonenum: 555_555_5555,
+      emailaddress: "samanthajones@gmail.com",
+    },
+    {
+      firstname: "John",
+      lastname: "Doe",
+      phonenum: 111_111_1111,
+      emailaddress: "johndoe@gmail.com",
+    },
+    {
+      firstname: "Jane",
+      lastname: "Smith",
+      phonenum: 222_222_2222,
+      emailaddress: "janesmith@gmail.com",
+    },
+  ];
+  
---
+++ assignment-5/src/app/models/contact.ts
@@ -0,0 +1,6 @@
+export interface Contact {
+    firstname: string;
+    lastname: string;
+    phonenum: number;
+    emailaddress: string;
+  }
